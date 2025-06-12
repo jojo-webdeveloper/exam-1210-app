4db570ee96e585d6bdd80db396544467a8a32354
Jojo De Jesus <jojo.webdeveloper@gmail.com>
2025-06-13 00:53:22 +0800

    Update table for migration command

--- database/migrations/2025_06_11_184538_create_user_task_table.php
+++ database/migrations/2025_06_11_184538_create_user_task_table.php
@@ -11,16 +11,16 @@ return new class extends Migration {
         Schema::create('user_task', function (Blueprint $table) {
             $table->id(); // id INT AUTO_INCREMENT PRIMARY KEY
             $table->unsignedBigInteger('parent_id')->nullable();
-            $table->string('title', 100);
-            $table->text('content');
-            $table->enum('task_status', ['done', 'inprogress', 'todo'])->nullable();
+            $table->string('title', 100); //required, max 100 characters
+            $table->text('content'); //required, no max length specified
+            $table->enum('task_status', ['done', 'inprogress', 'todo']); //required, can be 'done', 'inprogress', or 'todo'
             $table->enum('publish_status', ['draft', 'published', 'trashed'])->default('draft');
             $table->unsignedBigInteger('created_by');
 
             $table->dateTime('date_created')->default(DB::raw('CURRENT_TIMESTAMP'));
             $table->dateTime('date_updated')->nullable();
             $table->dateTime('date_published')->nullable();
-            $table->string('attachment', 200);
+            $table->string('attachment', 200)->nullable();
         });
     }
 
